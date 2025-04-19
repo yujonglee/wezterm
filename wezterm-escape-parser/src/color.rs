5306090f6cd74c6efe2279a63b38ed0501faf8c5
Wez Furlong <wez@wezfurlong.org>
2025-04-19 15:22:33 -0700

    extract wezterm-escape-parser into its own crate

---
+++ wezterm-escape-parser/src/color.rs
@@ -0,0 +1,254 @@
+use num_derive::FromPrimitive;
+#[cfg(feature = "use_serde")]
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+pub use wezterm_color_types::{LinearRgba, SrgbaTuple};
+use wezterm_dynamic::{FromDynamic, FromDynamicOptions, ToDynamic, Value};
+
+#[derive(Debug, Clone, Copy, FromPrimitive, PartialEq, Eq, FromDynamic, ToDynamic)]
+#[cfg_attr(feature = "use_serde", derive(Serialize, Deserialize))]
+#[repr(u8)]
+/// These correspond to the classic ANSI color indices and are
+/// used for convenience/readability in code
+pub enum AnsiColor {
+    /// "Dark" black
+    Black = 0,
+    /// Dark red
+    Maroon,
+    /// Dark green
+    Green,
+    /// "Dark" yellow
+    Olive,
+    /// Dark blue
+    Navy,
+    /// Dark purple
+    Purple,
+    /// "Dark" cyan
+    Teal,
+    /// "Dark" white
+    Silver,
+    /// "Bright" black
+    Grey,
+    /// Bright red
+    Red,
+    /// Bright green
+    Lime,
+    /// Bright yellow
+    Yellow,
+    /// Bright blue
+    Blue,
+    /// Bright purple
+    Fuchsia,
+    /// Bright Cyan/Aqua
+    Aqua,
+    /// Bright white
+    White,
+}
+
+impl From<AnsiColor> for u8 {
+    fn from(col: AnsiColor) -> u8 {
+        col as u8
+    }
+}
+
+/// Describes a color in the SRGB colorspace using red, green and blue
+/// components in the range 0-255.
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash)]
+pub struct RgbColor {
+    bits: u32,
+}
+
+impl Into<SrgbaTuple> for RgbColor {
+    fn into(self) -> SrgbaTuple {
+        self.to_tuple_rgba()
+    }
+}
+
+impl RgbColor {
+    /// Construct a color from discrete red, green, blue values
+    /// in the range 0-255.
+    pub const fn new_8bpc(red: u8, green: u8, blue: u8) -> Self {
+        Self {
+            bits: ((red as u32) << 16) | ((green as u32) << 8) | blue as u32,
+        }
+    }
+
+    /// Construct a color from discrete red, green, blue values
+    /// in the range 0.0-1.0 in the sRGB colorspace.
+    pub fn new_f32(red: f32, green: f32, blue: f32) -> Self {
+        let red = (red * 255.) as u8;
+        let green = (green * 255.) as u8;
+        let blue = (blue * 255.) as u8;
+        Self::new_8bpc(red, green, blue)
+    }
+
+    /// Returns red, green, blue as 8bpc values.
+    /// Will convert from 10bpc if that is the internal storage.
+    pub fn to_tuple_rgb8(self) -> (u8, u8, u8) {
+        (
+            (self.bits >> 16) as u8,
+            (self.bits >> 8) as u8,
+            self.bits as u8,
+        )
+    }
+
+    /// Returns red, green, blue as floating point values in the range 0.0-1.0.
+    /// An alpha channel with the value of 1.0 is included.
+    /// The values are in the sRGB colorspace.
+    pub fn to_tuple_rgba(self) -> SrgbaTuple {
+        SrgbaTuple(
+            (self.bits >> 16) as u8 as f32 / 255.0,
+            (self.bits >> 8) as u8 as f32 / 255.0,
+            self.bits as u8 as f32 / 255.0,
+            1.0,
+        )
+    }
+
+    /// Returns red, green, blue as floating point values in the range 0.0-1.0.
+    /// An alpha channel with the value of 1.0 is included.
+    /// The values are converted from sRGB to linear colorspace.
+    pub fn to_linear_tuple_rgba(self) -> LinearRgba {
+        self.to_tuple_rgba().to_linear()
+    }
+
+    /// Construct a color from an X11/SVG/CSS3 color name.
+    /// Returns None if the supplied name is not recognized.
+    /// The list of names can be found here:
+    /// <https://en.wikipedia.org/wiki/X11_color_names>
+    pub fn from_named(name: &str) -> Option<RgbColor> {
+        Some(SrgbaTuple::from_named(name)?.into())
+    }
+
+    /// Returns a string of the form `#RRGGBB`
+    pub fn to_rgb_string(self) -> String {
+        let (red, green, blue) = self.to_tuple_rgb8();
+        format!("#{:02x}{:02x}{:02x}", red, green, blue)
+    }
+
+    /// Returns a string of the form `rgb:RRRR/GGGG/BBBB`
+    pub fn to_x11_16bit_rgb_string(self) -> String {
+        let (red, green, blue) = self.to_tuple_rgb8();
+        format!(
+            "rgb:{:02x}{:02x}/{:02x}{:02x}/{:02x}{:02x}",
+            red, red, green, green, blue, blue
+        )
+    }
+
+    /// Construct a color from a string of the form `#RRGGBB` where
+    /// R, G and B are all hex digits.
+    /// `hsl:hue sat light` is also accepted, and allows specifying a color
+    /// in the HSL color space, where `hue` is measure in degrees and has
+    /// a range of 0-360, and both `sat` and `light` are specified in percentage
+    /// in the range 0-100.
+    pub fn from_rgb_str(s: &str) -> Option<RgbColor> {
+        let srgb: SrgbaTuple = s.parse().ok()?;
+        Some(srgb.into())
+    }
+
+    /// Construct a color from an SVG/CSS3 color name.
+    /// or from a string of the form `#RRGGBB` where
+    /// R, G and B are all hex digits.
+    /// `hsl:hue sat light` is also accepted, and allows specifying a color
+    /// in the HSL color space, where `hue` is measure in degrees and has
+    /// a range of 0-360, and both `sat` and `light` are specified in percentage
+    /// in the range 0-100.
+    /// Returns None if the supplied name is not recognized.
+    /// The list of names can be found here:
+    /// <https://ogeon.github.io/docs/palette/master/palette/named/index.html>
+    pub fn from_named_or_rgb_string(s: &str) -> Option<Self> {
+        RgbColor::from_rgb_str(&s).or_else(|| RgbColor::from_named(&s))
+    }
+}
+
+impl From<SrgbaTuple> for RgbColor {
+    fn from(srgb: SrgbaTuple) -> RgbColor {
+        let SrgbaTuple(r, g, b, _) = srgb;
+        Self::new_f32(r, g, b)
+    }
+}
+
+/// This is mildly unfortunate: in order to round trip RgbColor with serde
+/// we need to provide a Serialize impl equivalent to the Deserialize impl
+/// below.  We use the impl below to allow more flexible specification of
+/// color strings in the config file.  A side effect of doing it this way
+/// is that we have to serialize RgbColor as a 7-byte string when we could
+/// otherwise serialize it as a 3-byte array.  There's probably a way
+/// to make this work more efficiently, but for now this will do.
+#[cfg(feature = "use_serde")]
+impl Serialize for RgbColor {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = self.to_rgb_string();
+        s.serialize(serializer)
+    }
+}
+
+#[cfg(feature = "use_serde")]
+impl<'de> Deserialize<'de> for RgbColor {
+    fn deserialize<D>(deserializer: D) -> Result<RgbColor, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        RgbColor::from_named_or_rgb_string(&s)
+            .ok_or_else(|| format!("unknown color name: {}", s))
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+impl ToDynamic for RgbColor {
+    fn to_dynamic(&self) -> Value {
+        self.to_rgb_string().to_dynamic()
+    }
+}
+
+impl FromDynamic for RgbColor {
+    fn from_dynamic(
+        value: &Value,
+        options: FromDynamicOptions,
+    ) -> Result<Self, wezterm_dynamic::Error> {
+        let s = String::from_dynamic(value, options)?;
+        Ok(RgbColor::from_named_or_rgb_string(&s)
+            .ok_or_else(|| format!("unknown color name: {}", s))?)
+    }
+}
+
+/// An index into the fixed color palette.
+pub type PaletteIndex = u8;
+
+/// Specifies the color to be used when rendering a cell.
+/// This differs from `ColorAttribute` in that this type can only
+/// specify one of the possible color types at once, whereas the
+/// `ColorAttribute` type can specify a TrueColor value and a fallback.
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum ColorSpec {
+    Default,
+    /// Use either a raw number, or use values from the `AnsiColor` enum
+    PaletteIndex(PaletteIndex),
+    TrueColor(SrgbaTuple),
+}
+
+impl Default for ColorSpec {
+    fn default() -> Self {
+        ColorSpec::Default
+    }
+}
+
+impl From<AnsiColor> for ColorSpec {
+    fn from(col: AnsiColor) -> Self {
+        ColorSpec::PaletteIndex(col as u8)
+    }
+}
+
+impl From<RgbColor> for ColorSpec {
+    fn from(col: RgbColor) -> Self {
+        ColorSpec::TrueColor(col.into())
+    }
+}
+
+impl From<SrgbaTuple> for ColorSpec {
+    fn from(col: SrgbaTuple) -> Self {
+        ColorSpec::TrueColor(col)
+    }
+}
---
+++ wezterm-escape-parser/src/hyperlink.rs
@@ -0,0 +1,116 @@
+use crate::{Result, ensure, format_err};
+#[cfg(feature = "use_serde")]
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use wezterm_dynamic::{FromDynamic, ToDynamic};
+
+#[cfg_attr(feature = "use_serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, FromDynamic, ToDynamic)]
+pub struct Hyperlink {
+    params: HashMap<String, String>,
+    uri: String,
+    /// If the link was produced by an implicit or matching rule,
+    /// this field will be set to true.
+    implicit: bool,
+}
+
+impl Hyperlink {
+    pub fn uri(&self) -> &str {
+        &self.uri
+    }
+
+    pub fn compute_shape_hash<H: Hasher>(&self, hasher: &mut H) {
+        self.uri.hash(hasher);
+        for (k, v) in &self.params {
+            k.hash(hasher);
+            v.hash(hasher);
+        }
+        self.implicit.hash(hasher);
+    }
+
+    pub fn params(&self) -> &HashMap<String, String> {
+        &self.params
+    }
+
+    pub fn new<S: Into<String>>(uri: S) -> Self {
+        Self {
+            uri: uri.into(),
+            params: HashMap::new(),
+            implicit: false,
+        }
+    }
+
+    #[inline]
+    pub fn is_implicit(&self) -> bool {
+        self.implicit
+    }
+
+    pub fn new_implicit<S: Into<String>>(uri: S) -> Self {
+        Self {
+            uri: uri.into(),
+            params: HashMap::new(),
+            implicit: true,
+        }
+    }
+
+    pub fn new_with_id<S: Into<String>, S2: Into<String>>(uri: S, id: S2) -> Self {
+        let mut params = HashMap::new();
+        params.insert("id".into(), id.into());
+        Self {
+            uri: uri.into(),
+            params,
+            implicit: false,
+        }
+    }
+
+    pub fn new_with_params<S: Into<String>>(uri: S, params: HashMap<String, String>) -> Self {
+        Self {
+            uri: uri.into(),
+            params,
+            implicit: false,
+        }
+    }
+
+    pub fn parse(osc: &[&[u8]]) -> Result<Option<Hyperlink>> {
+        ensure!(osc.len() == 3, "wrong param count");
+        if osc[1].is_empty() && osc[2].is_empty() {
+            // Clearing current hyperlink
+            Ok(None)
+        } else {
+            let param_str = String::from_utf8(osc[1].to_vec())?;
+            let uri = String::from_utf8(osc[2].to_vec())?;
+
+            let mut params = HashMap::new();
+            if !param_str.is_empty() {
+                for pair in param_str.split(':') {
+                    let mut iter = pair.splitn(2, '=');
+                    let key = iter.next().ok_or_else(|| format_err!("bad params"))?;
+                    let value = iter.next().ok_or_else(|| format_err!("bad params"))?;
+                    params.insert(key.to_owned(), value.to_owned());
+                }
+            }
+
+            Ok(Some(Hyperlink::new_with_params(uri, params)))
+        }
+    }
+}
+
+impl core::fmt::Display for Hyperlink {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "8;")?;
+        for (idx, (k, v)) in self.params.iter().enumerate() {
+            // TODO: protect against k, v containing : or =
+            if idx > 0 {
+                write!(f, ":")?;
+            }
+            write!(f, "{}={}", k, v)?;
+        }
+        // TODO: ensure that link.uri doesn't contain characters
+        // outside the range 32-126.  Need to pull in a URI/URL
+        // crate to help with this.
+        write!(f, ";{}", self.uri)?;
+
+        Ok(())
+    }
+}
