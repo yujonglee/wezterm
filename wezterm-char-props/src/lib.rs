bb6534bd4b8e289af3ad129b001e97d39ca79012
Wez Furlong <wez@wezfurlong.org>
2025-04-20 07:58:41 -0700

    wezterm-char-props: allow building no_std

--- wezterm-char-props/src/lib.rs
+++ wezterm-char-props/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 pub mod emoji;
 pub mod emoji_presentation;
 pub mod emoji_variation;
--- wezterm-char-props/src/nerdfonts.rs
+++ wezterm-char-props/src/nerdfonts.rs
@@ -1,10 +1,14 @@
+#[cfg(feature = "std")]
 use std::collections::HashMap;
+#[cfg(feature = "std")]
 use std::sync::LazyLock;
 
+#[cfg(feature = "std")]
 pub static NERD_FONTS: LazyLock<HashMap<&'static str, char>> = LazyLock::new(build_map);
 
 pub use crate::nerdfonts_data::NERD_FONT_GLYPHS;
 
+#[cfg(feature = "std")]
 fn build_map() -> HashMap<&'static str, char> {
     crate::nerdfonts_data::NERD_FONT_GLYPHS
         .iter()
